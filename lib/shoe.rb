d32865eec3dc53ef5ae893e34f77ac490cacbe5e
Robin Kim <rkim1214@gmail.com>
2018-11-21 20:48:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(brand))
-      BRANDS << brand
-    end 
+    BRANDS << brand if !(BRANDS.include?(brand))
   end
 
   def cobble
